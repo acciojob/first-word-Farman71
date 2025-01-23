6701880b04f5eba4f7a8d709fd4d6ec423a96969
Farman71 <farmanhussain71@gmail.com>
2025-01-23 15:58:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function firstWord(s) {
-  // your code here
-}
-
+   //your code here
+	if (s === "") {
+        return "";
+    }
+ 	let str="";
+ 	let n=s.length;
+	for(i=0;i<n;i++)
+	{
+		if(s[i]===" ")
+		{
+			break;
+		}
+		str+=s[i];
+	}
+	return str;
+	
+ }
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
